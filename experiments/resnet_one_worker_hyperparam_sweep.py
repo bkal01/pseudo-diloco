ea5cfba6a145a6a4813885f2a87caf9f72253e22
Bhavesh Kalisetti <bhaveshkalisetti@gmail.com>
2025-06-04 00:06:10 -0700

    experiments: one worker resnet
    One worker resnet hyperparameter sweep on epochs, batch size, and outer_lr as these are the params im most unsure about in the diloco formulation for resnet
    
    pushing to github to run on gpu, so adding torch.compile as well.

---
+++ experiments/resnet_one_worker_hyperparam_sweep.py
@@ -0,0 +1,42 @@
+import wandb
+from src.pseudo_diloco.config import load_config_from_yaml, Config
+from src.pseudo_diloco.models.resnet import get_resnet_18
+from src.pseudo_diloco.train_resnet import train
+
+def train_with_config():
+    wandb.init()
+    config = load_config_from_yaml("configs/resnet-18-cifar-10.yml")
+    
+    config.training_config.num_epochs = wandb.config.epochs
+    config.training_config.per_replica_batch_size = wandb.config.per_replica_batch_size
+    config.outer_optimizer_config.lr = wandb.config.outer_lr
+    
+    model = get_resnet_18(
+        num_classes=config.architecture_config.num_classes,
+        img_size=config.dataset_config.img_size,
+    )
+    
+    train(model, config)
+
+if __name__ == "__main__":
+    sweep_config = {
+        "method": "grid",
+        "metric": {
+            "name": "val_acc",
+            "goal": "maximize"
+        },
+        "parameters": {
+            "epochs": {
+                "values": [200, 300]
+            },
+            "per_replica_batch_size": {
+                "values": [64, 128, 256]
+            },
+            "outer_lr": {
+                "values": [0.1, 0.4, 0.7]
+            }
+        }
+    }
+    
+    sweep_id = wandb.sweep(sweep_config, project="pseudo-diloco")
+    wandb.agent(sweep_id, function=train_with_config, count=18)
